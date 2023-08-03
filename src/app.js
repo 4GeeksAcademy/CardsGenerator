4686e897a51e5383ab595e2547261ab325895a81
Cristian Díaz <88348311+cristiandiazperdomo@users.noreply.github.com>
2023-08-03 14:09:52 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let iconsArray = ["fa-clover", "fa-spa", "fa-heart", "fa-diamond"];
+let numbersAndLetter = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const getRandomNumber = (max, min) =>
+  Math.floor(Math.random() * (max - min) + min);
+
+const showGeneratedCard = () => {
+  const iconsHTML = document.getElementsByTagName("i");
+
+  // GET ICON AND COLOR
+  let color = "";
+  const getIcon = iconsArray[getRandomNumber(0, iconsArray.length)];
+
+  // CHANGE CARD COLOR
+
+  for (let i = 0; i < iconsHTML.length; i++) {
+    getIcon === "fa-clover" || getIcon === "fa-spa"
+      ? (color = "black")
+      : (color = "red");
+    iconsHTML[i].classList.add(getIcon); // ASSING ICON
+    iconsHTML[i].style.color = color; // ASSING COLOR
+  }
+
+  // CHANGE NUMBER OR LETTER
+
+  const face = document.getElementById("face");
+  face.innerText =
+    numbersAndLetter[getRandomNumber(0, numbersAndLetter.length)];
 };
+
+showGeneratedCard();
